16555c9843c199c44733a0f27d8db1c05727ff1a
scissors-moth <scissorsmoth@gmail.com>
2026-06-10 16:22:15 -0400

    diminished breathing in critical state

--- Content.Server/Body/Systems/RespiratorSystem.cs
+++ Content.Server/Body/Systems/RespiratorSystem.cs
@@ -138,7 +138,19 @@ public sealed class RespiratorSystem : EntitySystem
                         respirator.Status = RespiratorStatus.Inhaling;
                         break;
                 }
-            }
+            }else{
+            switch (respirator.Status)
+                {
+                    case RespiratorStatus.Inhaling:
+                        Inhale((uid, respirator), 0.20f);
+                        respirator.Status = RespiratorStatus.Exhaling;
+                        break;
+                    case RespiratorStatus.Exhaling:
+                        Exhale((uid, respirator));
+                        respirator.Status = RespiratorStatus.Inhaling;
+                        break;
+                }
+		}
 
             if (respirator.Saturation < respirator.SuffocationThreshold)
             {
@@ -161,7 +173,7 @@ public sealed class RespiratorSystem : EntitySystem
         }
     }
 
-    public void Inhale(Entity<RespiratorComponent?> entity)
+    public void Inhale(Entity<RespiratorComponent?> entity, float multiplier = 1f)
     {
         if (!Resolve(entity, ref entity.Comp, logMissing: false))
             return;
@@ -178,7 +190,7 @@ public sealed class RespiratorSystem : EntitySystem
         if (ev.Gas is null)
             return;
 
-        var gas = ev.Gas.RemoveVolume(entity.Comp.BreathVolume);
+        var gas = ev.Gas.RemoveVolume(entity.Comp.BreathVolume*multiplier);
 
         var inhaleEv = new InhaledGasEvent(gas);
         RaiseLocalEvent(entity, ref inhaleEv);
