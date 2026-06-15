ff1886bd1ea2529b4769708cbf2183f1491e345c
Carcc <225926381+carccborg@users.noreply.github.com>
2026-06-15 11:51:56 -0400

    Update SleepingSystem.cs

--- Content.Shared/Bed/Sleep/SleepingSystem.cs
+++ Content.Shared/Bed/Sleep/SleepingSystem.cs
@@ -119,7 +119,11 @@ public sealed partial class SleepingSystem : EntitySystem
         {
             // Just in case we're not using the sleeping status
             EnsureComp<StunnedComponent>(ent);
-            EnsureComp<KnockedDownComponent>(ent);
+            // The entity will not fall over if they are buckled.
+            if (!EntityManager.TryGetComponent(ent, out BuckleComponent? buckleComp) || !buckleComp.Buckled)
+            {
+                EnsureComp<KnockedDownComponent>(ent);
+            }
 
             if (TryComp<SleepEmitSoundComponent>(ent, out var sleepSound))
             {
