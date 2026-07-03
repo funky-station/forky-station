82d458ce13972dc41004c31a9df4f9bf3fbb4b12
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-07-04 05:17:48 +1000

    Port: Cheap lighters which fail to light sometimes

---
+++ Content.Shared/_Funkystation/Tools/Components/CheapLighterComponent.cs
@@ -0,0 +1,28 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Audio;
+
+namespace Content.Shared._Funkystation.Tools.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CheapLighterComponent : Component
+{
+    /// <summary>
+    /// An additional sound the lighter should play when switched on, which can be interrupted when it's closed.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? SoundActivate;
+
+    /// <summary>
+    /// The chance the lighter will fail to light, between 0.0 and 1.0.
+    /// </summary>
+    [DataField]
+    public float FailChance = 0f;
+
+    /// <summary>
+    /// The sound the lighter will play when it fails to light.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? SoundFail;
+
+    public EntityUid? AudioStream;
+}
