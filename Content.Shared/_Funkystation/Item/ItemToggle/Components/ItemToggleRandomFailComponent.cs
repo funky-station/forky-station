d741488b69f1536512fefbc73c8519a6f999acab
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-07-18 06:03:48 +1000

    split cheaplighter into generic components, move funky content to funky folder

---
+++ Content.Shared/_Funkystation/Item/ItemToggle/Components/ItemToggleRandomFailComponent.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Funkystation.Item.ItemToggle.Components;
+
+/// <summary>
+/// Handles random fail events when attempting to activate an item.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ItemToggleRandomFailComponent : Component
+{
+    /// <summary>
+    /// The chance the item will fail to toggle on, between 0.0 and 1.0.
+    /// </summary>
+    [DataField]
+    public float FailChance = 0f;
+
+    [DataField]
+    public string? PopupText; // TODO: use localized strings
+}
