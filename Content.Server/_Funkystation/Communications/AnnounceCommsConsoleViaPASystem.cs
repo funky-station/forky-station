79ba56d9dc0485e23384b07d0fd0a3e02cf1248a
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-08-10 20:24:27 +1000

    move partial class extension to its own file

---
+++ Content.Server/_Funkystation/Communications/AnnounceCommsConsoleViaPASystem.cs
@@ -0,0 +1,50 @@
+using Content.Server._Funkystation.Communications;
+using Content.Shared.Communications;
+using Content.Shared.CCVar;
+using Content.Shared.Chat;
+
+namespace Content.Server.Communications
+{
+    public sealed partial class CommunicationsConsoleSystem
+    {
+        [Dependency] private PASystem _paSystem = null!;
+        private void AnnounceCommsConsoleViaPASystem(EntityUid uid,
+            CommunicationsConsoleComponent comp,
+            CommunicationsConsoleAnnounceMessage message)
+        {
+            Loc.TryGetString(comp.Title, out var title);
+            title ??= comp.Title;
+            title += " Announcement";
+
+            // if we're not going to include the name and ID of the sender, fall back to the "title" (i.e, Communications Console, Syndicate Nuclear Operative)
+            var author = comp.AnnounceSentBy ? Loc.GetString("comms-console-announcement-unknown-sender") : title;
+
+            if (message.Actor is { Valid: true } mob)
+            {
+                if (!CanAnnounce(comp))
+                    return;
+
+                if (!CanUse(mob, uid))
+                {
+                    _popupSystem.PopupEntity(Loc.GetString("comms-console-permission-denied"), uid, message.Actor);
+                    return;
+                }
+
+                if (comp.AnnounceSentBy)
+                    author = _identity.GetIdentityShortInfo(mob, uid) ?? author;
+            }
+
+            comp.AnnouncementCooldownRemaining = comp.Delay;
+            UpdateCommsConsoleInterface(uid, comp);
+
+            // i don't know if CommunicationConsoleAnnouncementEvent is actually used for anything, so we'll do it identically
+            // to a normal announcement just to be safe
+            var maxLength = _cfg.GetCVar(CCVars.ChatMaxAnnouncementLength);
+            var msg = SharedChatSystem.SanitizeAnnouncement(message.Message, maxLength);
+            var ev = new CommunicationConsoleAnnouncementEvent(uid, comp, msg, message.Actor);
+            RaiseLocalEvent(ref ev);
+
+            _paSystem.DispatchPAAnnouncement(message.Message, author, message.Actor, true, true, null, comp.Sound);
+        }
+    }
+}
