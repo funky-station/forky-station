5b1e55fccad739ce8679cde8b85e673b357ed7d3
Miguel4387 <mveramontoto@gmail.com>
2026-08-17 12:50:09 +0200

    thing

--- Content.Shared/_Funkystation/TraderCargo/Components/FTLCommsComputerComponent.cs
+++ Content.Shared/_Funkystation/TraderCargo/Components/FTLCommsComputerComponent.cs
@@ -4,5 +4,9 @@ namespace Content.Shared._Funkystation.TraderCargo.Components;
 [RegisterComponent]
 public sealed partial class FTLCommsComputerComponent : Component
 {
-
+    /// <summary>
+    /// The first trader shuttle in the FTL comms menu
+    /// </summary>
+    [DataField("traderShuttle")]
+    public EntityUid? TraderShuttle1 { get; set; }
 }
