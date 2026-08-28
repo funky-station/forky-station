7a30b683214eb9569d627e427e7deee2c45035e3
taydeo <tay@funkystation.org>
2026-08-28 15:46:57 -0700

    start creating clipboard menu

---
+++ Content.Client/_Funkystation/Unions/UnionClipboardBoundUserInterface.cs
@@ -0,0 +1,17 @@
+﻿using Content.Client._Funkystation.Unions.UI;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._Funkystation.Unions;
+
+public sealed class UnionClipboardBoundUserInterface(EntityUid owner, Enum uiKey, UnionClipboardMenu? menu)
+    : BoundUserInterface(owner, uiKey)
+{
+    [ViewVariables] private UnionClipboardMenu? _menu = menu;
+    
+    protected override void Open()
+    {
+        base.Open();
+        
+        _menu = this.CreateWindow<UnionClipboardMenu>();
+    }
+}
---
+++ Content.Shared/_Funkystation/Unions/UnionEvents.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._Funkystation.Unions;
+
+[Serializable, NetSerializable]
+public enum UnionClipboardUiKey : byte
+{
+    Key,
+}
