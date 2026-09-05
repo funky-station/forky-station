8d3ddf6f1d097b27e741b407857d230b8a9b81b3
DevilishMilk <michaellapjr@gmail.com>
2026-09-05 14:28:50 -0400

    placeholder prototype

--- Content.Shared/Slippery/SlipperyComponent.cs
+++ Content.Shared/Slippery/SlipperyComponent.cs
@@ -47,10 +47,10 @@ namespace Content.Shared.Slippery
         public SlipperyEffectEntry SlipData = new();
 
         /// <summary>
-        /// FUNKYSTATION - Whether or not this entity respects NoSlipComponent.
+        /// FUNKYSTATION - Whether this entity respects NoSlipComponent.
         /// </summary>
         [DataField, AutoNetworkedField]
-        public bool IgnoreNoSlip = false;
+        public bool IgnoreNoSlip;
     }
     /// <summary>
     /// Stores the data for slipperiness that way reagents and this component can use it.
