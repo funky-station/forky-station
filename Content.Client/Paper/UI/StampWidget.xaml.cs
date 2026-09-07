2c97a937ca731a4b2f863a958d2cf8dceca4778a
emilo <emil.o.johansen@gmail.com>
2026-09-07 18:15:06 +0200

    implemented mycobiota's fix

--- Content.Client/Paper/UI/StampWidget.xaml.cs
+++ Content.Client/Paper/UI/StampWidget.xaml.cs
@@ -55,7 +55,7 @@ public sealed partial class StampWidget : PanelContainer
     {
         RobustXamlLoader.Load(this);
         var prototypes = IoCManager.Resolve<IPrototypeManager>();
-        _stampShader = prototypes.Index<ShaderPrototype>("PaperStamp").InstanceUnique();
+        _stampShader = prototypes.Index<ShaderPrototype>(PaperStamp).InstanceUnique();
 
         if (PanelOverride != null)
              return;
