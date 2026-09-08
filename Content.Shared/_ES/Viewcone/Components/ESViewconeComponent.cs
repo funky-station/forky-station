1f1ba16c2e620e464a2a5476a30d30dc13882d4a
lukachukai <219865375+lukachukai2@users.noreply.github.com>
2026-09-08 15:34:05 -0400

    why did I add a space

--- Content.Shared/_ES/Viewcone/Components/ESViewconeComponent.cs
+++ Content.Shared/_ES/Viewcone/Components/ESViewconeComponent.cs
@@ -45,7 +45,6 @@ public sealed partial class ESViewconeComponent : Component
     [DataField, AutoNetworkedField]
     public float ConeFeather = 10f;
 
-
     [DataField, AutoNetworkedField]
     public float ConeIgnoreRadius = 1f; //Funky 0.5f -> 1f
 
