0ad5c9dd54457eccc05bdb3153d921cf1eefa72f
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-09-12 23:26:57 +1000

    missed a spot

--- Content.Server/Chat/Managers/ChatManager.cs
+++ Content.Server/Chat/Managers/ChatManager.cs
@@ -420,6 +420,7 @@ internal sealed partial class ChatManager : IChatManager
         if (IsValidWarpDestination(source) && ShouldShowFollowButton(recipient))
         {
             var btnText = _localizationManager.GetString("chat-manager-follow-button");
+            // funky - using a unique ghostfollow command link tag as part of a fix for chat stacking
             return $"[ghostfollow=\"{btnText}\" command=\"{GhostFollowEntityCommand.CommandName} {_entityManager.GetNetEntity(source)}\" /] " + wrappedMessage;
         }
 
