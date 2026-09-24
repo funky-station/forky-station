a200ccea8cfa9c28c06d36227f5bcf43cc020ef3
mycobiota <154991750+mycobiota@users.noreply.github.com>
2026-09-24 14:12:57 +1000

    downstream comments

--- Content.Shared/Labels/EntitySystems/LabelSystem.cs
+++ Content.Shared/Labels/EntitySystems/LabelSystem.cs
@@ -167,9 +167,11 @@ public sealed partial class LabelSystem : EntitySystem
             }
 
             args.PushMarkup(Loc.GetString("comp-paper-label-has-label"));
+            // funky start - using permissive parser for paper labels so that invalid tags don't cause a crash
             var message = new FormattedMessage();
             message.AddMarkupPermissive(paper.Content.TrimEnd());
             args.PushMessage(message);
+            // funky end
         }
     }
 
